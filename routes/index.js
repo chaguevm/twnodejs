536f153d81c8bf280b157a5ab4426ded81dda140
Darwin Valero <devm_4648@hotmail.com>
2020-09-02 12:46:49 -0500

    Fix para perfiles no encontrados, ahora renderiza la vista de 404

--- routes/index.js
+++ routes/index.js
@@ -31,26 +31,30 @@ router.get('/profile/:username', isLoggedIn, (req, res) => {
 
     getUser(username).then((result) => {
         profile = result;
-        pool.query(`SELECT id FROM follows WHERE user_follower = ${req.user.id} AND user_followed = ${result.id}`, (err, result) => {
-            if (err) throw err;
-            if (req.user.id === profile.id)
-                isFollowing = true;
-            else if (result.length > 0)
-                isFollowing = true;
-            else
-                isFollowing = false;
-
-            const query = `SELECT (SELECT COUNT(user_follower) as cant FROM follows JOIN users ON user_followed = users.id WHERE users.username = '${username}' ) AS followers, (SELECT COUNT(user_followed) as cant FROM follows JOIN users ON user_follower = users.id WHERE users.username = '${username}' ) AS followings`;
-            pool.query(query, (err, result) => {
+        if(profile !== undefined){
+            pool.query(`SELECT id FROM follows WHERE user_follower = ${req.user.id} AND user_followed = ${result.id}`, (err, result) => {
                 if (err) throw err;
-                stats = result[0];
-                pool.query(`SELECT * FROM tweets JOIN users ON user_id = users.id WHERE users.username = '${username}' ORDER BY tweets.id DESC `, (err, result) => {
+                if (req.user.id === profile.id)
+                    isFollowing = true;
+                else if (result.length > 0)
+                    isFollowing = true;
+                else
+                    isFollowing = false;
+
+                const query = `SELECT (SELECT COUNT(user_follower) as cant FROM follows JOIN users ON user_followed = users.id WHERE users.username = '${username}' ) AS followers, (SELECT COUNT(user_followed) as cant FROM follows JOIN users ON user_follower = users.id WHERE users.username = '${username}' ) AS followings`;
+                pool.query(query, (err, result) => {
                     if (err) throw err;
-                    tweets = result;
-                    res.render('usertweets', { tweets, _user: profile, stats });
+                    stats = result[0];
+                    pool.query(`SELECT * FROM tweets JOIN users ON user_id = users.id WHERE users.username = '${username}' ORDER BY tweets.id DESC `, (err, result) => {
+                        if (err) throw err;
+                        tweets = result;
+                        res.render('usertweets', { tweets, _user: profile, stats });
+                    });
                 });
             });
-        });
+        }else{
+            res.render('404');
+        }
     }).catch(err => {
         throw err;
     });
