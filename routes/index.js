3bd83e2c157bc60f77416580bb4a0fe2fc702c4d
Darwin Valero <devm_4648@hotmail.com>
2020-09-02 00:42:27 -0500

    Resuelto el error de la ruta perfil

--- routes/index.js
+++ routes/index.js
@@ -30,53 +30,37 @@ router.get('/profile/:username', isLoggedIn, (req, res) => {
     let profile, isFollowing, stats, tweets;
 
     getUser(username).then((result) => {
-        console.log('Usuario consultado');
-        console.log(result);
-        if (req.user.id === result.id) {
-            isFollowing = true; //Es el mismo usuario
-        } else {
-            pool.query(`SELECT id FROM follows WHERE user_follower = ${req.user.id} AND user_followed = ${result.id}`, (err, result) => {
+        profile = result;
+        pool.query(`SELECT id FROM follows WHERE user_follower = ${req.user.id} AND user_followed = ${result.id}`, (err, result) => {
+            if (err) throw err;
+            console.log(req.user.id);
+            console.log(profile.id);
+            console.log(result);
+            if (req.user.id === profile.id)
+                isFollowing = true;
+            else if (result.length > 0)
+                isFollowing = true;
+            else
+                isFollowing = false;
+
+            const query = `SELECT (SELECT COUNT(user_follower) as cant FROM follows JOIN users ON user_followed = users.id WHERE users.username = '${username}' ) AS followers, (SELECT COUNT(user_followed) as cant FROM follows JOIN users ON user_follower = users.id WHERE users.username = '${username}' ) AS followings`;
+            pool.query(query, (err, result) => {
                 if (err) throw err;
-                if (result.length > 0)
-                    isFollowing = true;
-                else
-                    isFollowing = false;
-                console.log("Se siguen o no se siguen");
-                console.log(result);
-                const query = `SELECT (SELECT COUNT(user_follower) as cant FROM follows JOIN users ON user_followed = users.id WHERE users.username = '${username}' ) AS followers, (SELECT COUNT(user_followed) as cant FROM follows JOIN users ON user_follower = users.id WHERE users.username = '${username}' ) AS followings`;
-                pool.query(query, (err, result) => {
+                stats = result[0];
+                console.log("estadisticas");
+                console.log(stats);
+                pool.query(`SELECT * FROM tweets JOIN users ON user_id = users.id WHERE users.username = '${username}' ORDER BY tweets.id DESC `, (err, result) => {
                     if (err) throw err;
-                    stats = result[0];
-                    console.log("estadisticas");
-                    console.log(stats);
-                    pool.query(`SELECT * FROM tweets JOIN users ON user_id = users.id WHERE users.username = '${username}' ORDER BY tweets.id DESC `, (err, result) => {
-                        if (err) throw err;
-                        tweets = result;
-                        console.log('tweets');
-                        console.log(tweets);
-                        res.render('usertweets', { tweets, _user: profile, stats });
-                    });
+                    tweets = result;
+                    console.log('tweets');
+                    console.log(tweets);
+                    res.render('usertweets', { tweets, _user: profile, stats });
                 });
             });
-        }
+        });
     }).catch(err => {
         throw err;
     });
-
-
-
-    /*
-    pool.query(`SELECT * FROM users WHERE username = '${username}'`, (err, result ) => {
-        if (err) throw err;
-        profile = result[0];
-        /*pool.query(`SELECT id FROM follows WHERE user_follower = ${req.user.id} AND user_followed = ${profile.id}`, (err, result) => {
-            if (err) throw err;
-            isFollowing = result[0];
-            
-        //});
-    });
-    */
-
 });
 
 module.exports = router;
